9851c002ff91a94b8a1201550a6ac206288175c2
Aman <109422024+Amannaza@users.noreply.github.com>
2023-01-04 02:56:38 +0300

    update1 Water class

--- src/Water.java
+++ src/Water.java
@@ -1,1 +1,1 @@
-public class Water {    public static void main(String[] args) {        System.out.println("Water is amazing");    }}
+public class Water {    public static void main(String[] args) {        System.out.println("Water is amazing");        System.out.println("Drink mineral water not coke");    }}
