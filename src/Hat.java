8216613f3d1155bac0a68693d0c1a1289123ece1
Aman <109422024+Amannaza@users.noreply.github.com>
2023-01-04 05:01:22 +0300

    updated2 hat class

--- src/Hat.java
+++ src/Hat.java
@@ -1,1 +1,1 @@
-public class Hat {    public static void main(String[] args) {        System.out.println("Hat class cretaed");    }}
+public class Hat {    public static void main(String[] args) {        System.out.println("Hat class cretaed");        System.out.println("No idea about types of hat");    }}
