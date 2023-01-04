0cd4783e33e32998b3ee02c34d6bc673e7525285
Aman <109422024+Amannaza@users.noreply.github.com>
2023-01-04 05:07:41 +0300

    updated2 store class

--- src/Store.java
+++ src/Store.java
@@ -1,1 +1,1 @@
-public class Store {    public static void main(String[] args) {        System.out.println("We opened just a store class");    }}
+public class Store {    public static void main(String[] args) {        System.out.println("We opened just a store class");        System.out.println("Updates");    }}
